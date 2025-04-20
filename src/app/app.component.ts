b8db9acd67c16d5ef13e65dabc826e95d934dbf9
adriatls <adria.tavares28@gmail.com>
2025-04-20 11:17:51 -0300

    feat: add logo images and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet, ButtonModule],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MenubarModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'instituto';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    this.headerItens = [
+      {
+        label: 'O Instituto',
+      },
+      {
+        label: 'Ações',
+      },
+      {
+        label: 'Eventos',
+      },
+      {
+        label: 'Como doar',
+      },
+      {
+        label: 'Parceiros',
+      },
+      {
+        label: 'Contato',
+      },
+    ];
+  }
+
+  protected headerItens: MenuItem[] | undefined;
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { appConfigTheme } from '../app.config';
 
 export const appConfig: ApplicationConfig = {
@@ -10,6 +9,5 @@ export const appConfig: ApplicationConfig = {
     ...appConfigTheme.providers,
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
   ],
 };
