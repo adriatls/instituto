a460002a26ccb267371a60b1be4e393f56affecf
adriatls <adria.tavares28@gmail.com>
2025-04-21 10:15:01 -0300

    feat: moved header logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuItem } from 'primeng/api';
-import { MenubarModule } from 'primeng/menubar';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenubarModule],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  ngOnInit() {
-    this.headerItens = [
-      {
-        label: 'O Instituto',
-      },
-      {
-        label: 'Ações',
-      },
-      {
-        label: 'Eventos',
-      },
-      {
-        label: 'Como doar',
-      },
-      {
-        label: 'Parceiros',
-      },
-      {
-        label: 'Contato',
-      },
-    ];
-  }
-
-  protected headerItens: MenuItem[] | undefined;
-}
+export class AppComponent {}
