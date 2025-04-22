57a0a1caeb386a2db1a9a4855b36d141bbc321da
adriatls <adria.tavares28@gmail.com>
2025-04-22 08:21:32 -0300

    feat: add footer to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ContactComponent, AboutComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ContactComponent,
+    AboutComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
