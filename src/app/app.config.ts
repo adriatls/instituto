b7be5fc9700cb4047a3699aa1f4f6276be745a21
adriatls <adria.tavares28@gmail.com>
2025-12-21 10:48:38 -0300

    refator: simplified app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { appConfigTheme } from '../app.config';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+
+const appConfigTheme: ApplicationConfig = {
+  providers: [
+      provideAnimationsAsync(),
+      providePrimeNG({
+          theme: {
+              preset: Aura,
+              options: {
+                  cssLayer: {
+                      name: 'primeng',
+                      order: 'theme, base, primeng'
+                  }
+              }
+          }
+      })
+  ]
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
