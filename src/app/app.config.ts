c1c359dee95688d64c21640da4627fe60c937b42
adriatls <adria.tavares28@gmail.com>
2025-12-21 11:44:18 -0300

    chore: upgrade Angular and related dependencies to version 20

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,26 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { provideRouter } from '@angular/router';
 
 const appConfigTheme: ApplicationConfig = {
   providers: [
-      provideAnimationsAsync(),
-      providePrimeNG({
-          theme: {
-              preset: Aura,
-              options: {
-                  cssLayer: {
-                      name: 'primeng',
-                      order: 'theme, base, primeng'
-                  }
-              }
-          }
-      })
-  ]
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng',
+          },
+        },
+      },
+    }),
+  ],
 };
 
 export const appConfig: ApplicationConfig = {
