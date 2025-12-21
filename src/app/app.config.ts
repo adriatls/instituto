92f6867b1ff2389585e008c6e9a3e0b6243f834f
adriatls <adria.tavares28@gmail.com>
2025-12-21 12:07:08 -0300

    chore: upgrade Angular and related dependencies to version 21

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,11 @@
+import { providePrimeNG } from 'primeng/config';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { routes } from './app.routes';
-import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 const appConfigTheme: ApplicationConfig = {
   providers: [
-    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
