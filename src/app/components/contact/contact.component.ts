403fb248a34ea648c5cff2c3675f31da8a76a20f
adriatls <adria.tavares28@gmail.com>
2025-12-21 11:04:33 -0300

    refactor: remove contact form functionality

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,82 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ContactFormGroup, initializeContactForm } from './contact.form';
-import { TextareaModule } from 'primeng/textarea';
+import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
-import { send as emailjsSend } from '@emailjs/browser';
-import { environment } from '../../../environments/environment';
-import { MessageService } from 'primeng/api';
-import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-contact',
-  imports: [
-    ReactiveFormsModule,
-    TextareaModule,
-    ButtonModule,
-    InputTextModule,
-    FloatLabelModule,
-    ToastModule,
-  ],
-  providers: [MessageService],
+  imports: [ButtonModule, FloatLabelModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent implements OnInit {
-  protected contactForm: ContactFormGroup | undefined;
-  protected lodingSentMessage: boolean = false;
-
-  public constructor(private readonly messageService: MessageService) {}
-
-  public ngOnInit() {
-    this.contactForm = initializeContactForm();
-  }
-
-  protected sentMessage() {
-    this.lodingSentMessage = true;
-    const contact = {
-      name: this.contactForm?.controls.name.value,
-      email: this.contactForm?.controls.email.value,
-      message: this.contactForm?.controls.message.value,
-    };
-
-    emailjsSend(
-      environment.emailJs.serviceId,
-      environment.emailJs.templateId,
-      contact,
-      {
-        publicKey: environment.emailJs.publicKey,
-      }
-    ).then(
-      () => {
-        this.showMessageSuccess();
-        this.lodingSentMessage = false;
-        this.contactForm?.reset();
-      },
-      () => {
-        this.showMessageError();
-      }
-    );
-  }
-
-  private showMessageSuccess() {
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Sucesso',
-      detail: 'Mensagem enviada!',
-      life: 3000,
-    });
-  }
-
-  private showMessageError() {
-    this.messageService.add({
-      severity: 'warn',
-      summary: 'Erro',
-      detail:
-        'Não foi possível enviar sua mensagem! Por favor, tente mais tarde.',
-      life: 3000,
-    });
-  }
-}
+export class ContactComponent {}
